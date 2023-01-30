8823614980a3df259ae1d7c022b028b250dc8f89
Lucheek <Lucheek@users.noreply.github.com>
2023-01-30 00:59:45 +0000

    deploy: 0cf8a184e3a2c532c98eacf917fb88a36c2f5ac7

---
+++ assets/latin.html-4d5afe2f.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-f65b159c","path":"/events/latin.html","title":"Vestibulum et aliquet risus, quis volutpat lacus","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1675040359000,"contributors":[{"name":"Lucheek","email":"123911685+Lucheek@users.noreply.github.com","commits":1}]},"filePathRelative":"events/latin.md"}');export{e as data};
